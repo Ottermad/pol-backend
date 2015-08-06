287e57efdd7b78dbfe25115d3ebb463f9a6a283f
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-08-06 14:30:30 +0100

    added exists route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
 
+from .user.views import user
+
 app = Flask()
 app.config['DEBUG'] = True
+
+app.register_blueprint(user)
--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-from .functions import user_exists
+from .functions import user_exists, get_constituency
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
 
-@user.route("/exists/<id>")
+@user.route('/exists/<id>')
 def exists(id):
-    user_exists(id)
+    does_user_exist = user_exists(id)
+    data = {
+        'exists': does_user_exist
+    }
+    return jsonify
+
+@user.route('/new/<long>/<lat>')
+def new(long, lat):
+    constituency = get_constituency(long, lat)
